89a05ffe1fbe1809b5b0e9d62547919cad63703b
nammar1999 <nammar@missouriwestern.edu>
2025-03-10 12:35:10 -0500

    Updated the interface screen with more detailed options for the teller, manager and customer. Will update those screens further with more input later on.

--- demo2/src/ManagerScreen.java
+++ demo2/src/ManagerScreen.java
@@ -1,11 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ManagerScreen extends JFrame {
     public ManagerScreen() {
         setTitle("Manager Screen");
         setSize(300, 200);
-        JLabel label = new JLabel("Welcome to Manager Screen", SwingConstants.CENTER);
-        add(label);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(4, 1));
+
+        JButton loanButton = new JButton("Loan Interest Rates");
+        JButton cdButton = new JButton("CD Interest Rates");
+        JButton cdmanageButton = new JButton("CD Manager");
+        JButton checksButton = new JButton("Checks Processing");
+        JButton returnButton = new JButton("Return to Controller");
+
+        loanButton.addActionListener(e -> new SystemControllerScreen());
+        cdButton.addActionListener(e -> new TellerScreen());
+        cdmanageButton.addActionListener(e -> new ManagerScreen());
+        checksButton.addActionListener(e -> new CustomerScreen());
+        returnButton.addActionListener(e -> new SystemControllerScreen());
+
+        add(loanButton);
+        add(cdButton);
+        add(cdmanageButton);
+        add(checksButton);
+        add(returnButton);
+
         setVisible(true);
     }
 }
