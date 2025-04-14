97c81d9a0766e0f52c388ea780877b620d0c3837
JamesFosterIV <jpfosteriv@gmail.com>
2025-04-13 23:36:18 -0500

    Small update to the WithdrawScreen.java

--- demo2/src/WithdrawScreen.java
+++ demo2/src/WithdrawScreen.java
@@ -24,24 +24,26 @@ public class WithdrawScreen extends JFrame {
         // Labels and text fields
         add(new JLabel("Please enter the customer's Social Security Number: "));
         ssn = new JTextField();
-        String SSN = ssn.getText().trim();
         add(ssn);
 
         add(new JLabel("Please enter the amount to withdraw: "));
         withdraw = new JTextField();
-        Double amount = Double.parseDouble(withdraw.getText().trim());
         add(withdraw);
 
 
         add(new JLabel("Please enter the account number you wish to withdraw from: "));
         account = new JTextField();
-        Long accountID = Long.parseLong(account.getText().trim());
         add(account);
 
         JButton submitButton = new JButton("Withdraw from Account");
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String SSN = ssn.getText().trim();
+                double amount = Double.parseDouble(withdraw.getText().trim());
+                long accountID = Long.parseLong(account.getText().trim());
+
+
                 makeWithdrawal(SSN, amount, accountID);
             }
         });
@@ -58,33 +60,6 @@ public class WithdrawScreen extends JFrame {
         setVisible(true);
     }
 
-    private void withdrawAccount() {
-        String withdrawAccount = withdraw.getText().trim();
-        String accountNumber = account.getText().trim();
-        String socialsecurity = ssn.getText().trim();
-
-        if (withdrawAccount.isEmpty() || accountNumber.isEmpty() || socialsecurity.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Must complete required fields.", "Error", JOptionPane.ERROR_MESSAGE);
-            return;
-        } else {
-
-            //Create the Customer Object and add it to the Array List
-            //That way we can access the customer objects methods and what not later on
-            // Account account = new Account(account1,account2);
-            //Database.addItemToList(Database.accountList, account);
-
-            // Store the data to database
-            // might have to change txt name - not created yet!!!!!!!!!!!!
-            try{ /*(FileWriter writer = new FileWriter("customers.txt", true)) {
-                writer.write(withdraw + ";" + account + ";" + ssn + ";" + "\n");*/
-                JOptionPane.showMessageDialog(this, "Withdrawal Completed Successfully!");
-                //dispose();
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(this, "Error Withdrawing from Account!", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-
-        }//end of else
-    }
 
     public void makeWithdrawal(String SSN, double amount, Long accountID){
         Customer customer = Database.getCustomerFromList(SSN);
