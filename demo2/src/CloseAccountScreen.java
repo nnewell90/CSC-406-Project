ffa1e66e72580b61e0666257b310a6fffbcfd31c
JamesFosterIV <jpfosteriv@gmail.com>
2025-04-30 10:57:16 -0500

    Fixed close Account screen to delete all types of accounts.

--- demo2/src/CloseAccountScreen.java
+++ demo2/src/CloseAccountScreen.java
@@ -41,11 +41,13 @@ public class CloseAccountScreen extends JFrame {
             JOptionPane.showMessageDialog(null, "Please Enter Account ID");
         } else {
 
+
+
             long accountID = Long.parseLong(accountIDField.getText());
             AbstractAccount account = Database.getAccountFromList(Database.abstractAccountList, accountID);
 
             if (account == null) {
-                JOptionPane.showMessageDialog(null, "Account Not Found, check Credentials");
+                JOptionPane.showMessageDialog(this, "Account Not Found, check Credentials");
             } else {
 
                 if (account.accountType == AbstractAccount.AccountType.CheckingAccount) {//checking accounts
@@ -53,53 +55,44 @@ public class CloseAccountScreen extends JFrame {
 
                     calculateRemaining(checkingAccount);
                     CheckingAccount.deleteAccount(checkingAccount);
-                    JOptionPane.showMessageDialog(null, "Account #" + accountID + " Deleted");
+                    JOptionPane.showMessageDialog(this, "Account #" + accountID + " Deleted");
                     dispose();
                     new TellerScreen();
-                } else if (account.accountType == AbstractAccount.AccountType.SavingsAccount) {//savings accounts
 
-                    if (account instanceof SavingsAccount.SimpleSavingsAccount ssa) {//simple savings
-                        calculateRemaining(ssa);
-                        SavingsAccount.SimpleSavingsAccount.deleteAccount(ssa);
-                        JOptionPane.showMessageDialog(null, "Account #" + accountID + " Deleted");
-                        dispose();
-                        new TellerScreen();
-
-                    } else if (account instanceof SavingsAccount.CDSavingsAccount cdAccount) {//CD savings
-                        LocalDate today = LocalDate.now();
-
-                        int amount = (int) cdAccount.getBalance();
-                        //Don't let them withdraw too much from the cd account
-
-                        if (today.isAfter(cdAccount.dueDate) || today.equals(cdAccount.dueDate)) {
-
-                            cdAccount.withdrawAfterDueDate(amount);
-                            JOptionPane.showMessageDialog(null, "CD Account #" + account.getAccountID() + " is closed. Customer is owed $" + amount);
-                            dispose();
-                            new TellerScreen();
-
-                        } else if (today.isBefore(cdAccount.dueDate)) {//if they withdraw before the maturation date, proceed but penalize
-
-                            //apply "penalty"
-                            JOptionPane.showMessageDialog(this, "CD account Closed! However, " +
-                                    "a penalty was applied for closing before " + cdAccount.dueDate + ". Customer is owed $" + amount);
-
-                            cdAccount.withdrawBeforeDueDate(amount);
-
-                            JOptionPane.showMessageDialog(null, "Account #" + accountID + " Deleted");
-                            dispose();
-                            new TellerScreen();
-                        }
-
-                    }//end of CD savings if
+                } else if (account instanceof SavingsAccount.SimpleSavingsAccount ssa) {//Simple savings accounts
+                    calculateRemaining(ssa);
+                    SavingsAccount.SimpleSavingsAccount.deleteAccount(ssa);
+                    JOptionPane.showMessageDialog(this, "Account #" + accountID + " Deleted");
+                    dispose();
+                    new TellerScreen();
 
 
-                }else if (account.accountType == AbstractAccount.AccountType.ShortOrLongLoanAccount) {//Loan account
+                } else if (account instanceof SavingsAccount.CDSavingsAccount cdAccount){//CD account
 
-                    LoanAccount.ShortOrLong loanAccount = (LoanAccount.ShortOrLong) account;
+                    LocalDate today = LocalDate.now();
+                    int amount = (int) cdAccount.getBalance();
+
+                    if (today.isAfter(cdAccount.dueDate) || today.equals(cdAccount.dueDate)) {
+                        cdAccount.withdrawAfterDueDate(amount);
+                        SavingsAccount.CDSavingsAccount.deleteAccount(cdAccount);
+                        JOptionPane.showMessageDialog(this, "CD Account #" + account.getAccountID() + " is closed. Customer is owed $" + amount);
+                    } else if (today.isBefore(cdAccount.dueDate)) {//if they withdraw before the maturation date, proceed but penalize
+                        //apply "penalty"
+                        JOptionPane.showMessageDialog(this, "CD account Closed! However, " +
+                                "a penalty was applied for closing before " + cdAccount.dueDate + ". Customer is owed $" + amount);
+                        cdAccount.withdrawBeforeDueDate(amount);
+                        SavingsAccount.CDSavingsAccount.deleteAccount(cdAccount);
+                        JOptionPane.showMessageDialog(this, "Account #" + accountID + " Deleted");
+                    }
+
+                    dispose();
+                    new TellerScreen();
+
+                }else if (account instanceof LoanAccount.ShortOrLong loanAccount) {//SHort or Long Loan account
+
                     calculateRemaining(loanAccount);
                     LoanAccount.ShortOrLong.deleteAccount(loanAccount);
-                    JOptionPane.showMessageDialog(null, "Account #" + accountID + " Deleted");
+                    JOptionPane.showMessageDialog(this, "Account #" + accountID + " Deleted");
                     dispose();
                     new TellerScreen();
 
@@ -107,13 +100,12 @@ public class CloseAccountScreen extends JFrame {
                     LoanAccount.CC creditCard = (LoanAccount.CC) account;
                     calculateRemaining(creditCard);
                     LoanAccount.CC.deleteAccount(creditCard);
-                    JOptionPane.showMessageDialog(null, "Account #" + accountID + " Deleted");
+                    JOptionPane.showMessageDialog(this, "Account #" + accountID + " Deleted");
                     dispose();
                     new TellerScreen();
                 }else {
-                    JOptionPane.showMessageDialog(null, "Cannot delete this type of account.");
+                    JOptionPane.showMessageDialog(this, "Cannot delete this type of account.");
                 }
-
 
             }
         }
